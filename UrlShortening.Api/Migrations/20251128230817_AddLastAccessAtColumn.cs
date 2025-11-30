c9b4dddf9dba4178b324048984f11e8e9b4b12f9
Antoni Ortiz <antonigarcia.og@gmail.com>
2025-11-29 22:57:36 -0600

    Add LastAccessAt field to UrlMapping

---
+++ UrlShortening.Api/Migrations/20251128230817_AddLastAccessAtColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortening.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastAccessAtColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastAccessAt",
+                table: "UrlMappings",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAccessAt",
+                table: "UrlMappings");
+        }
+    }
+}
--- UrlShortening.Api/Models/UrlMapping.cs
+++ UrlShortening.Api/Models/UrlMapping.cs
@@ -6,9 +6,10 @@
         
         public string OriginalUrl { get; set; } = string.Empty;
         public string Code { get; set; } = string.Empty;
-
-        public int HitCount { get; set; } = 0;
         
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int HitCount { get; set; } = 0;
+        public DateTime? LastAccessAt { get; set; }
     }
 }
